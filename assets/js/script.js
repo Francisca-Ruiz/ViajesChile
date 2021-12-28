f3f4351a903269308c9278b386e5206f3685393c
Francisca Ruiz <panchaa.ruiz@gmail.com>
2021-12-28 16:36:53 -0300

    llevame a cada seccion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
+$(document).ready(function () {
+    console.log("entra aquí");
+    // TOOLTIPS aparece etiqueda de mensaje
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    // Enviar correo alerta
+    $( ".enviarMensaje" ).click(function() {
+    alert( "El mensaje fue enviado correctamente" );
+
+    });
+
+
+
+            //Toggle aparece y desaparece
+    $( ".card-title" ).click(function() {
+      $( ".card-text" ).toggle( "slow", function() {
+      });
+    });
+
+});    
+    
+    
+    
     $(document).ready(function(){
+
+        // Scroll
 		$(window).on("scroll",function(){
-		var changeColor = $(window).scrollTop();
+		    var changeColor = $(window).scrollTop();
 			    if(changeColor > 0){
 			    	$(".navbar").css("background","rgba(0,0,0)");
 
@@ -10,4 +37,5 @@
                    
 			    }
 			  });
-			});
+            });
+            
