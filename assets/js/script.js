a699764973ae9ad83ac567a268dc58c1f43fc460
Francisca Ruiz <panchaa.ruiz@gmail.com>
2021-12-28 15:28:43 -0300

    cambios visuales css y efectos js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+		$(window).on("scroll",function(){
+		var changeColor = $(window).scrollTop();
+			    if(changeColor > 0){
+			    	$(".navbar").css("background","rgba(0,0,0)");
+
+			    }
+			    else{
+			    	$(".navbar").css("background","rgba(250,250,250,0)");
+                   
+			    }
+			  });
+			});
