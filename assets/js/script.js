5f1b1c041053440c7da41170c19f7a11cf556948
Francisca Ruiz <panchaa.ruiz@gmail.com>
2021-12-29 00:08:50 -0300

    orden directorio

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,15 @@ $(document).ready(function () {
     // Mensaje enviado alerta
     $( ".enviarMensaje" ).click(function() {
     alert( "El mensaje fue enviado correctamente" );
-
     });
     // barra de navegación
     $(window).on("scroll",function(){
         var changeColor = $(window).scrollTop();
-            if(changeColor > 0){
-                $(".navbar").css("background","rgba(0,0,0)");
-
-            }
-            else{
-                $(".navbar").css("background","rgba(250,250,250,0)");
-               
+        if(changeColor > 0){
+    $(".navbar").css("background","rgba(0,0,0)");
+        }
+        else{
+        $(".navbar").css("background","rgba(250,250,250,0)");    
             }
           });
 
